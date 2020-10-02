1766553a3cec52d44c48fa53b6d8bfe72bcb8b11
landryfomena <landryfomena09@gmail.com>
2020-10-02 15:43:11 +0100

    undo deleted notes

--- app/src/main/java/com/kola/cleannotes/business/interactions/notelist/RestoreDeletedNote.kt
+++ app/src/main/java/com/kola/cleannotes/business/interactions/notelist/RestoreDeletedNote.kt
@@ -1,3 +1,86 @@
 package com.kola.cleannotes.business.interactions.notelist
 
-class RestoreDeletedNote
+import com.kola.cleannotes.business.data.cache.CacheResponseHandler
+import com.kola.cleannotes.business.data.cache.abstraction.NoteCacheDataSource
+import com.kola.cleannotes.business.data.network.abstraction.NoteNetworkDataSource
+import com.kola.cleannotes.business.data.util.safeCacheCall
+import com.kola.cleannotes.business.domain.model.Note
+import com.kola.cleannotes.business.domain.state.*
+import com.kola.cleannotes.framework.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class RestoreDeletedNote(
+    private val noteCacheDataSource: NoteCacheDataSource,
+    private val noteNetworkDataSource: NoteNetworkDataSource
+) {
+    fun retoreDeletedNote(
+        note: Note,
+        stateEvent: StateEvent
+    ): Flow<DataState<NoteListViewState>?> = flow {
+        val cacheResult = safeCacheCall(IO) {
+            noteCacheDataSource.insertNote(note)
+        }
+
+        val response = object : CacheResponseHandler<NoteListViewState, Long>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override suspend fun handleSuccess(resultObj: Long): DataState<NoteListViewState>? {
+                return if (resultObj > 0) {
+                    val viewState = NoteListViewState(
+                        notePendingDelete = NoteListViewState.NotePendingDelete(
+                            note = note
+                        )
+
+                    )
+                    DataState.data(
+                        response = Response(
+                            message = RESTORE_NOTE_SUCCESS,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Success()
+                        ),
+                        data = viewState,
+                        stateEvent = stateEvent
+
+
+                    )
+                } else {
+                    DataState.data(
+                        response = Response(
+                            message = RESTORE_NOTE_FAILED,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Success()
+                        ),
+                        data = null,
+                        stateEvent = stateEvent
+
+
+                    )
+                }
+            }
+        }.getResult()
+
+        emit(response)
+
+    }
+
+    private suspend fun updateNetwork(response: String?,note: Note) {
+        if (response.equals(RESTORE_NOTE_SUCCESS)) {
+            //insert into the note node
+            safeCacheCall(IO) {
+                noteNetworkDataSource.insertOrUpdateNote(note)
+            }
+            // remove from the deletes node
+            safeCacheCall(IO) {
+                noteNetworkDataSource.deleteDeletedNote(note)
+            }
+        }
+    }
+
+    companion object {
+        const val RESTORE_NOTE_SUCCESS = "Successfully restored the deleted note."
+        const val RESTORE_NOTE_FAILED = "Failed to restore the deleted note."
+    }
+}
